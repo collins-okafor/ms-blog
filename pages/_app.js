526b7794fbb5a20dd05b0d3ef6feda456b14584c
Emmyzealous <emmygazzillion123@gail.com>
2022-11-17 21:12:18 +0100

    started_blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../store.js";
-
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../lib/globalStyles";
+import { lightTheme } from "../lib/theme";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Montserrat:wght@600&family=PT+Serif:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
   );
 }
 
