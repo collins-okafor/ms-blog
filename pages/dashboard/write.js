92a45bf048e7a818dd7a69f83d4de07e464e2d3d
Joshua Ejike <joshuaejike4221@gmail.com>
2022-12-08 19:51:02 +0100

    resent

--- pages/dashboard/write.js
+++ pages/dashboard/write.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useState } from "react";
+import { useEffect } from "react";
+import DashbaordPageWrapper from "../../universal-Components/DashobardPageWrapper";
 
 const Write = () => {
+  const [position, setPosition] = useState();
+
+  function getLocation() {
+    window.navigator.geolocation.getCurrentPosition(
+      (newPos) => setPosition(newPos),
+      console.error
+    );
+  }
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <DashbaordPageWrapper>
+      <div>
+        <CKEditor editor={ClassicEditor} onInit={(editor) => {}} />
+      </div>
+    </DashbaordPageWrapper>
   );
 };
 
