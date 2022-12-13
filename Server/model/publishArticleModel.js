55f15f58f1e83a52b39573be9ba9c3a1577b6509
Joshua@4221 <joshuaejike4221@gmail.com>
2022-12-13 15:56:01 +0100

    resent work

--- Server/model/publishArticleModel.js
+++ Server/model/publishArticleModel.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
 
-const ArticleSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    require: [true, "please provide title"],
-  },
+const ArticleSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: [true, "please provide title"],
+    },
 
-  article: {
-    type: String,
-    require: [true, "please provide article"],
-  },
+    article: {
+      type: String,
+      require: [true, "please provide article"],
+    },
 
-  tag: {
-    type: String,
-    require: [true, "please provide tag"],
-  },
+    tag: {
+      type: String,
+      require: [true, "please provide tag"],
+    },
 
-  cover_pic: {
-    type: String,
-    require: [true, "please provide tag"],
-  },
+    cover_pic: {
+      type: String,
+      require: [true, "please provide tag"],
+    },
 
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: [true, "Please provide user"],
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
   },
-
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 const ArticleSchemaState =
   mongoose.models.Articles || mongoose.model("Articles", ArticleSchema);
--- pages/api/article.js
+++ pages/api/article.js
@@ -1,12 +1,24 @@
 import { StatusCodes } from "http-status-codes";
 import connectDB from "../../Server/db/connect";
 import ArticleSchemaState from "../../Server/model/publishArticleModel";
+import jwt from "jsonwebtoken";
 
 export default async function handler(req, res) {
   await connectDB();
 
   if (req.method === "POST") {
     const authHeader = req.headers.authorization;
+
+    if (
+      !req.body.title ||
+      !req.body.article ||
+      !req.body.tag ||
+      !req.body.cover_pic
+    ) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "all field must be filled" });
+    }
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res
@@ -17,5 +29,15 @@ export default async function handler(req, res) {
     const token = authHeader.split(" ")[1];
 
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(payload, "resent payload");
+
+    const article = await ArticleSchemaState.create({
+      ...req.body,
+      username: payload.username,
+      email: payload.email,
+    });
+
+    res.status(200).json({ data: article });
   }
 }
