d81cc61b54d9ae18f2036949ef6a7a6d378b58d5
Joshua@4221 <joshuaejike4221@gmail.com>
2022-12-14 07:12:37 +0100

    resent work to add

--- services/writeService.js
+++ services/writeService.js
@@ -1,12 +1,12 @@
 import APIs from "./api";
 
-const WriteService = {
-  postArticle: async (params) => {
-    return APIs.post(`/api/read/article`, params).then((data) => {
-      console.log(data, "resent work");
-      return data.data.data;
-    });
-  },
-};
+// const WriteService = {
+//   postArticle: async (params) => {
+//     return APIs.post(`/api/read/article`, params).then((data) => {
+//       console.log(data, "resent work");
+//       return data.data.data;
+//     });
+//   },
+// };
 
 export default WriteService;
